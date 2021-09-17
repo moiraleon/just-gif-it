9e01f6fa99ac9b6e2797f151cbd5b83b8811e947
Moira Leon <moira.leon2@gmail.com>
2021-09-17 12:08:21 -0600

    Demo tuning Auto Configuration

--- src/main/java/schultz/dustin/io/JustGifItApplication.java
+++ src/main/java/schultz/dustin/io/JustGifItApplication.java
@@ -3,7 +3,14 @@ package schultz.dustin.io;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,7 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import javax.annotation.PostConstruct;
 import java.io.File;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class, JmxAutoConfiguration.class, WebSocketAutoConfiguration.class})
 public class JustGifItApplication {
 
     @Value("${multipart.location}/gif/")
@@ -28,6 +35,30 @@ public class JustGifItApplication {
             gifFolder.mkdir();
         }
     }
+
+    @Bean
+          public FilterRegistrationBean deRegisterHiddenHttpMethodFilter (HiddenHttpMethodFilter filter){
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterHttpPutFormContentFilter (HttpPutFormContentFilter filter){
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterRequestContextFilter (RequestContextFilter filter){
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+
+
 
     @Bean
     public WebMvcConfigurer webMvcConfigurer() {
